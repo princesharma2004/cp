f9d66a2b373410f0f1b753c2c79d0405af601dc0
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2025-08-12 18:39:59 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifndef LOCAL
+#define cerr if(false) cerr
+#endif
+
+void run_case(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        run_case();
+    }
+    return 0;
+}
